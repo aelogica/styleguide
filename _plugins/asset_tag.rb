9b4e303d6aedf037fa01ada51e9da965051d5294
Cecille Manalang <dev+cecille@aelogica.com>
2012-07-31 17:07:57 +0800

    WIP: assets and links handling for both prod and dev envs

---
+++ _plugins/asset_tag.rb
@@ -0,0 +1,54 @@
+# from http://mikeferrier.com/2011/04/29/blogging-with-jekyll-haml-sass-and-jammit/
+class AssetTag < Liquid::Tag
+
+  CONFIG = YAML.load_file("_config.yml")
+
+  def initialize(tag_name, name, kind, tokens)
+    super tag_name, name, tokens
+    @name   = name.to_s.strip
+    @kind   = kind.to_s
+  end
+
+  def render(context)
+    if Jekyll::ENV == 'production'
+      markup "/#{CONFIG['site']['base_url']}/#{CONFIG['site']['project_name']}/#{CONFIG[@asset_type]}/#{name_with_ext}"
+    else
+      markup "#{@path}/#{name_with_ext}"
+    end
+  end
+
+  def name_with_ext
+    "#{@name}.#{@ext}"
+  end
+
+end
+
+class IncludeJsTag < AssetTag
+  def initialize(tag_name, name, tokens)
+    @path = '/javascripts'
+    @ext = 'js'
+    @asset_type = 'javascripts'
+    super tag_name, name, 'js', tokens
+  end
+
+  def markup(src)
+    %{ <script src='#{src}' type='text/javascript'></script> }.to_s
+  end
+end
+
+Liquid::Template.register_tag('include_js', IncludeJsTag)
+
+class IncludeCssTag < AssetTag
+  def initialize(tag_name, name, tokens)
+    @path = '/stylesheets'
+    @ext = 'css'
+    @asset_type = 'stylesheets'
+    super tag_name, name, 'css', tokens
+  end
+
+  def markup(src)
+    %{ <link href='#{src}' media='screen' rel='stylesheet' type='text/css'> }.to_s
+  end
+end
+
+Liquid::Template.register_tag('include_css', IncludeCssTag)
